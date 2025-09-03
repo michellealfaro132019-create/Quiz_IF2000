23dc3346a5cfc15b2948e922767c0591ec7b39c8
Shannell Bennit <bennitshannell@gmail.com>
2025-09-02 19:20:41 -0600

    Quiz_4

---
+++ Ejercicio_Quiz/Main_Ejercicio_quiz.java
@@ -0,0 +1,8 @@
+package Ejercicio_Quiz;
+
+public class Main_Ejercicio_quiz {
+    public static void main(String[] args) {
+        Ejercicio_quiz quiz = new Ejercicio_quiz();
+        quiz.quiz_piramide(5);
+    }
+}
